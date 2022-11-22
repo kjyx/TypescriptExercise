721a9d717ba98fd4b6056afdfe99ed7563ff3eeb
kangjianye <1287158160@qq.com>
2022-11-22 09:28:00 +0800

    easy-pick add js implementation

--- questions/easy-pick/template.ts
+++ questions/easy-pick/template.ts
@@ -1,3 +1,18 @@
+
+// ts 实现
 type MyPick<T, K extends keyof T> = {
     [key in K]: T[key];
 };
+
+// js 实现
+
+// function MyPick(T, K) {
+//     let obj = {};
+//     for (let key in K) {
+//         if (key in T) {
+//             obj[key] = T[key];
+//         }
+//     }
+
+//     return obj;
+// }
