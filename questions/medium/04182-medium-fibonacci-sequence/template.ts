0f4be47c0630784d24d7cf6a48a2173a285233f8
kangjianye <1287158160@qq.com>
2023-07-10 07:42:12 +0800

    2023/7/9

--- questions/medium/04182-medium-fibonacci-sequence/template.ts
+++ questions/medium/04182-medium-fibonacci-sequence/template.ts
@@ -1,3 +1,8 @@
-type Fibonacci<T extends number> = any
+// 三个值 current记录当前正确的数 n left_1 记录n-1  left_2 记录n-2
+
+type Fibonacci<T extends number, current extends any[] = [1, 1, 1], left_1 extends any[] = [1], left_2 extends any[] = [1]> =
+    T extends 1 | 2 ? 1 : T extends current['length'] ? [...left_1, ...left_2]['length'] : Fibonacci<T, [...current, 1], left_2, [...left_1, ...left_2]>
 
 
+
+type asdsadas = Fibonacci<6>
